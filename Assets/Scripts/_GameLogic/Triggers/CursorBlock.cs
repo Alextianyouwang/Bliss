ff3623236caf910a957584af177782980868fa21
Alextianyouwang <54587384+Alextianyouwang@users.noreply.github.com>
2023-01-02 16:27:22 -0800

    Integrete_01/02/2023

--- Assets/Scripts/_GameLogic/Triggers/CursorBlock.cs
+++ Assets/Scripts/_GameLogic/Triggers/CursorBlock.cs
@@ -65,7 +65,7 @@ public class CursorBlock : NumberBlocks
         }
         if (collision.gameObject.tag.Equals("Restart")) 
         {
-            UnityEngine.SceneManagement.SceneManager.LoadScene("Bliss");
+            UnityEngine.SceneManagement.SceneManager.LoadScene("Bliss_Terrain Remodel");
             Cursor.lockState = CursorLockMode.None;
             //AudioManager.instance.StopAllSound();
         }
